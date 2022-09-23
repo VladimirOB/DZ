bbb4c6c3b1918f3ecb9b688e905b341ff5aefb44
VladimirOB <vovaso4@gmail.com>
2022-09-23 20:13:35 +0300

    11

---
+++ CS/DZ 23.09.22(ExtMethod_Event)/CS_Events/CS_Events/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Timers;
+// объявление типа данных делегат
+delegate int Subscriber(string str);
+
+class Server
+{
+    // создание переменной-списка адресов функций (event)
+    protected event Subscriber subsribers;
+    System.Timers.Timer timer;
+    int interval;
+
+    public Server(int interval)
+    {
+        this.interval = interval;
+    }
+
+    public void Start()
+    {
+        timer = new System.Timers.Timer(interval);
+        timer.Elapsed += Timer_Elapsed;
+        timer.Start();
+        for (int i = 0; i < 1000; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(1000);
+        }
+        timer.Stop();
+    }
+
+    private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        // старт события на сервере
+        this.StartEvent("Alarm !!! Fire !!!");
+
+        // старт события на сервере
+        this.StartEvent("Disk failure!");
+    }
+
+    // метод, подписывающий клиентские классы на сообщения
+    public void Add(Subscriber ev)
+    {
+        // оператор += добавляет ссылку на метод в event
+        subsribers += ev;
+    }
+
+    // метод, отписывающий клиентские классы на сообщения
+    public void Remove(Subscriber ev)
+    {
+        // оператор -= удаляет ссылку на метод из event
+        subsribers -= ev;
+    }
+
+    // метод, стартующий событие и вызывающий обработчики события в классах-подписчиках
+    public void StartEvent(string str)
+    {
+        // если есть подписчики, то сообщить им о событии (вызвать их обработчики)
+        /*if(subsribers != null)
+            subsribers(str);*/
+
+        subsribers?.Invoke(str);
+    }
+}
+
+// первый класс-подписчик
+class Client1                                       
+{
+    // обработчик события
+    public int OnEvent(string str)
+    {
+        Console.WriteLine("Client 1: {0}", str);
+        return 0;
+    }
+}
+
+// второй класс подписчик
+class Client2
+{
+    // обработчик события
+    public int OnGetEvent(string str)
+    {
+        Console.WriteLine("Client 2: {0}", str);
+        return 0;
+    }
+}
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        // создание подписчиков и сервера
+        Client1 c1 = new Client1();
+        Client2 c2 = new Client2();
+        Server server = new Server(500);
+        // подписка на события
+        server.Add(c1.OnEvent);
+        server.Add(c2.OnGetEvent);
+        server.Add(c1.OnEvent);
+        server.Remove(c1.OnEvent);
+
+        server.Start();
+
+        //// старт события на сервере
+        //server.StartEvent("Alarm !!! Fire !!!");
+
+        //// старт события на сервере
+        //server.StartEvent("Disk failure!");
+    }
+
+   
+}
---
+++ CS/DZ 23.09.22(ExtMethod_Event)/CS_ExtensionMethods/CS_ExtensionMethods/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CS_ExtensionMethods
+{
+    // при добавлении Extension Methods нужно создать именно static class
+    public static class StringExt
+    {
+        // Extension Method имеет то имя, которое будет добавлено в класс-получатель
+        // this - признак того, что это Extension Method
+        // string - название класса-получателя
+        // str - ссылка на текущий объект класса
+        public static int VowelCount(this string str)
+        {
+            int result = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ("AEUOIYaeuoiy".Contains(str[i]))
+                    result++;
+            }
+            return result;
+        }
+    }
+
+    public enum Car
+    {
+        BMW, Mercedes, Audi, Nissan, Opel, AstonMartin, VW, Ferrary, Subaru
+    }
+
+    // класс содержит метод расширения для enum
+    public static class CarExt
+    {
+        public static IEnumerable GetValues(this Car car)
+        {
+            return Enum.GetValues(typeof(Car));
+        }
+    }
+
+    // класс содержит метод расширения для кортежа
+    public static class TupleExt
+    {
+        // метод расширения, который позволяет получить структуру Point из определённого кортежа
+        public static Point ToPoint(this (double x, double y) self) => new Point(self.x, self.y);
+
+        // метод расширения, который позволяет получить определённого кортеж из структуры Point
+        public static (double x, double y) ToTuple(this Point self) => (self.X, self.Y);
+    }
+
+	public static class MyTestExt
+	{
+		public static void Print2(this Test test)
+		{
+            Console.WriteLine(@"Extension method!!!");
+		}
+	}
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello world";
+            Console.WriteLine($"Vowel Count: {s.VowelCount()}");
+
+            Car car = Car.BMW;
+            foreach (var item in car.GetValues())
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+
+            // создание экземпляра структуры Point
+            var p = new Point(10, 20);
+ 
+            // конвертирование структуры в кортеж
+            var (x, y) = p.ToTuple();
+
+            // конвертирование кортежа в структуру
+            var p2 = (x, y).ToPoint();
+
+            Test test = new Test();
+            test.Print2();
+        }
+    }
+}
