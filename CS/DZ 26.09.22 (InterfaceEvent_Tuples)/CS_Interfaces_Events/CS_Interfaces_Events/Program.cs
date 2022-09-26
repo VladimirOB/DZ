ea71bab8052d13241abe35d32eef0e77cbcf5aef
VladimirOB <vovaso4@gmail.com>
2022-09-26 23:31:53 +0300

    12

---
+++ CS/DZ 26.09.22 (InterfaceEvent_Tuples)/CS_Interfaces_Events/CS_Interfaces_Events/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+interface IReceiver
+{
+    int OnReceive(string str);
+}
+
+class Server
+{
+    // создание переменной-списка адресов функций (event)
+    public List<IReceiver> subscribers { get; } = new List<IReceiver>();
+
+    // метод, подписывающий клиентские классы на сообщения
+    public void Add(IReceiver ev)
+    {
+        subscribers.Add(ev);
+    }
+
+    // метод, отписывающий клиентские классы на сообщения
+    public void Remove(IReceiver ev)
+    {
+        subscribers.Remove(ev);
+    }
+
+    // метод, стартующий событие и вызывающий обработчики события в классах-подписчиках
+    public void StartEvent(string str)
+    {
+        // если есть подписчики, то сообщить им о событии (вызвать их обработчики)
+        foreach (var current in subscribers)
+        {
+            current.OnReceive(str);
+        }
+    }
+}
+
+// первый класс-подписчик
+class Client1: IReceiver
+{
+    // обработчик события
+    int IReceiver.OnReceive(string str)
+    {
+        Console.WriteLine("Client 1: {0}", str);
+        return 0;
+    }
+}
+
+// второй класс подписчик
+class Client2: IReceiver
+{
+	// обработчик события
+	int IReceiver.OnReceive(string str)
+    {
+        Console.WriteLine("Client 2: {0}", str);
+        return 0;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // создание подписчиков и сервера
+        Client1 c1 = new Client1();
+        Client2 c2 = new Client2();
+        Server server = new Server();
+
+        // подписка на события
+        server.Add(c1);
+        server.Add(c2);
+        server.Add(c1);
+        server.Remove(c1);
+
+        // старт события на сервере
+        server.StartEvent("Alarm !!! Fire !!!");
+
+        // старт события на сервере
+        server.StartEvent("Disk failure!");
+    }
+}
+
---
+++ CS/DZ 26.09.22 (InterfaceEvent_Tuples)/CS_Tuples/CS_Tuples/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Tuples
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name = null, string surname = null, int age = 0)
+        {
+            FirstName = name;
+            LastName = surname;
+            Age = age;
+        }
+
+        // стандартный метод, который позволяет присваивать объект текущего класса в кортеж с переменными
+        public void Deconstruct(out string name, out string surname, out int age)
+        {
+            name = FirstName;
+            surname = LastName;
+            age = Age;
+        }
+
+        public void Deconstruct(out string name, out string surname)
+        {
+            name = FirstName;
+            surname = LastName;
+        }
+
+        // оператор приведения типа из кортежа в Person
+        public static implicit operator Person((string name, string surname, int age) x) 
+            => new Person(x.name, x.surname, x.age);
+
+        /*public static implicit operator Person((string name, string surname, int age) x)
+        {
+            return new Person(x.name, x.surname, x.age);
+        }*/
+        
+        public void Print()
+        {
+            Console.WriteLine($"First name: {FirstName}, Last name: {LastName}, Age: {Age}");
+        }
+    }
+    
+    class Program
+    {
+        private static (int x, int y) PointOffset((int x, int y) point, int offsetX, int offsetY)
+        {
+            return (point.x + offsetX, point.y + offsetY);
+        }
+
+        static void Main(string[] args)
+        {
+            // объявление кортежа
+            var tuple = (1, 2, 3);
+            Console.WriteLine(tuple);
+
+            // обращение к элементам
+            Console.WriteLine(tuple.Item1);
+            Console.WriteLine(tuple.Item2);
+            tuple.Item1 += 9;
+            Console.WriteLine(tuple.Item1);
+
+            Console.WriteLine(tuple);
+
+            // объявление с указанием типов
+            (string, string, int, int) address = ("Donetsk", "Lenina", 6, 12);
+            address.Item3 = 9;
+            Console.WriteLine(address);
+
+            // объявление с указанием имён
+            var address2 = (city: "Moskow", street: "Gorky park", house: 3, number: 5);
+            Console.WriteLine(address2.city);
+            Console.WriteLine(address2.street);
+
+            // объявление с указанием типов и имён
+            (string city, string street, int house, int number) address3 = ("Moskow", "Gorky park", 3, 5);
+            address3.number = 7;
+            Console.WriteLine(address3);
+
+            // объявление кортежа из одиночных переменных
+            var (firstName, lastName, height) = ("Alex", "Petrov", 180);
+            firstName = "Ivan";
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(height);
+            Console.WriteLine((firstName, lastName, height));
+
+            // присваивание кортежей
+            address = address2;
+
+            // сравнение кортежей
+            Console.WriteLine($"Tuple compare: {address == address2}");
+
+            // передача и возвращение кортежа из метода
+            var point = PointOffset((1, 2), 3, 4);
+            Console.WriteLine($"Point with offset: {point}");
+
+            // использование в коллекциях
+            var p = (x: 1, y: 2);
+
+            var hashSet = new HashSet<(int x, int y)>();
+            hashSet.Add(p);
+            hashSet.Add((45, 67));
+
+            p.x++;
+            Console.WriteLine(hashSet.Contains(p));
+
+            foreach(var current in hashSet)
+            {
+                Console.WriteLine(current);
+            }
+
+            var person = new Person("Alex", "Petrov", 34);
+
+            // вызов метода Deconstruct класса Person
+            var (name, surname, age) = person;
+            
+            // создание нового кортежа на основе переменных
+            var alex = (name, surname, age);
+            
+            // вызов оператора приведения типов в классе Person
+            Person person2 = alex;
+            person2.Print();
+        }
+    }
+}
