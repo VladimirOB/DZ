c36ed0a8252be84684731d02ecba98bc7c75d208
VladimirOB <vovaso4@gmail.com>
2022-10-10 18:51:48 +0300

    mem

---
+++ DP/DZ 10.10.22(Memento_Prototype)/DP_Memento/DP_Memento/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_Memento
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Класс имеет начальные данные
+            SalesProspect s = new SalesProspect();
+            s.Name = "Noel van Halen";
+            s.Phone = "(412) 256-0990";
+            s.Budget = 25000.0;
+
+            // Инициализация хранилища резервных копий
+            ProspectMemory memory = new ProspectMemory();
+
+            // Выполнение back-up
+            memory.Memento = s.SaveMemento();
+
+            // Изменение свойств основного объекта
+            s.Name = "Leo Welch";
+            s.Phone = "(310) 209-7111";
+            s.Budget = 1000000.0;
+
+            // Восстановление данных основного объекта из резервной копии
+            s.RestoreMemento(memory.Memento);
+
+            // Wait for user
+            Console.ReadKey();
+
+        }
+
+    }
+
+    // Класс, данные которого нужно сохранять
+    class SalesProspect
+    {
+        private string _name;
+        private string _phone;
+        private double _budget;
+
+        // Gets or sets name
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                Console.WriteLine("Name:  " + _name);
+            }
+        }
+
+        // Gets or sets phone
+        public string Phone
+        {
+            get { return _phone; }
+            set
+            {
+                _phone = value;
+                Console.WriteLine("Phone: " + _phone);
+            }
+        }
+
+        // Gets or sets budget
+
+        public double Budget
+        {
+            get { return _budget; }
+            set
+            {
+                _budget = value;
+                Console.WriteLine("Budget: " + _budget);
+            }
+        }
+
+        // Выполнение back-up
+        public Memento SaveMemento()
+        {
+            Console.WriteLine("\nSaving state --\n");
+            return new Memento(_name, _phone, _budget);
+        }
+
+        // Восстановление данных из back-up
+        public void RestoreMemento(Memento memento)
+        {
+            Console.WriteLine("\nRestoring state --\n");
+            this.Name = memento.Name;
+            this.Phone = memento.Phone;
+            this.Budget = memento.Budget;
+        }
+    }
+
+    // Контейнер, позволяющий хранить данные (резервная копия)
+    class Memento
+    {
+        private string _name;
+        private string _phone;
+        private double _budget;
+
+        // Constructor
+        public Memento(string name, string phone, double budget)
+        {
+            this._name = name;
+            this._phone = phone;
+            this._budget = budget;
+        }
+
+        // Gets or sets name
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // Gets or set phone
+
+        public string Phone
+        {
+            get { return _phone; }
+            set { _phone = value; }
+        }
+
+        // Gets or sets budget
+
+        public double Budget
+        {
+            get { return _budget; }
+            set { _budget = value; }
+        }
+    }
+
+    // Хранилище резервных копий
+    class ProspectMemory
+    {
+        private Memento _memento;
+
+        // Property
+        public Memento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+}
+
+
+
---
+++ DP/DZ 10.10.22(Memento_Prototype)/DP_Prototype/Prototype/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyPrototype
+{
+    class MainApp
+    {
+        static void Main()
+        {
+            Game mygame = new Game();
+            mygame.Start();
+            mygame.print();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Game
+    {
+        Field field = new Field();
+
+        string GetId()
+        {
+            return "TR237848";
+        }
+
+        // Объявление экземпляра прототипа
+        Car prototype;
+
+        public Game()
+        {
+            // Создание прототипа при помощи конструктора
+            prototype = new GameCar(this.GetId(), "Nissan", 23, 345, 25, 250);
+        }
+
+        Random r = new Random();
+
+        // генерация поля игры, содержащего много клонируемых объектов
+        public void Start()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                // Генерация нового объекта при помощи клонирования прототипа
+                GameCar c1 = (GameCar)prototype.Clone();
+
+                // Изменение свойств клонированного объекта при помощи случайных значений
+                c1.Speed = r.Next(120, 250);
+                c1.Mass = r.Next(1000, 2500);
+                if (r.Next(2) == 0)
+                    c1.Model = "BMW";
+                else c1.Model = "Mercedes";
+
+                field.Add(c1);
+            }
+        }
+
+        public void print()
+        {
+            field.print();
+        }
+    }
+
+    class Field
+    {
+        List<Car> cars = new List<Car>();
+
+        public void Add(Car c)
+        {
+            cars.Add(c);
+        }
+
+        public void print()
+        {
+            foreach (Car c in cars)
+            {
+                c.print();
+            }
+        }
+    }
+
+    // абстрактный класс прототипа
+    abstract class Car
+    {
+
+        private string _id;
+        int color;
+
+        public int Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+        string model;
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        int mass;
+
+        public int Mass
+        {
+            get { return mass; }
+            set { mass = value; }
+        }
+        int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public Car(string id)
+        {
+            this._id = id;
+        }
+
+        public string Id
+        {
+
+            get { return _id; }
+        }
+
+        public virtual void print()
+        {
+
+        }
+
+        // Объявление метода, который клонирует прототип
+        public abstract Car Clone();
+    }
+
+    // конкретный прототип
+    class GameCar: Car
+    {
+        public GameCar(string id) : base(id)
+        {
+
+        }
+
+        public int MaxSpeed { get; set; }
+
+        public GameCar(string id, string model, int color, int mass, int speed, int maxSpeed) : base(id)
+        {
+            this.MaxSpeed = maxSpeed;
+            this.Color = color;
+            this.Mass = mass;
+            this.Speed = speed;
+            this.Model = model;
+        }
+
+        public override void print()
+        {
+            Console.WriteLine("Brand: {0} Speed: {1} Weight: {2} Id: {3} MaxSpeed: {4}", Model, Speed, Mass, Id, MaxSpeed);
+        }
+
+        // реализация клонирования прототипа
+        public override Car Clone()
+        {
+            // чаще всего здесь производится глубокое клонирование
+            return (Car)this.MemberwiseClone();
+        }
+
+    }
+}
